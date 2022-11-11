e4bab079b346e2b84a61dd53b8af4aab34409eb0
Fabian Munoz <fabianfa.munoz@gmail.com>
2022-11-11 11:30:20 -0500

    Update

---
+++ src/controllers/matter_controller.js
@@ -0,0 +1,45 @@
+const matter_model = require("../models/matter_model")
+
+module.exports = {
+    addMatter: async (req, res) => {
+        try {
+            const matter = new matter_model(req.body)
+            const result = await matter.save()
+            res.status(201).send(result)
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    },
+    getMatter: async (req, res) => {
+        try {
+            const result = await matter_model.find()
+            res.status(200).send(result)
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    },
+    getMatterById: async (req, res) => {
+        try {
+            const result = await matter_model.findById(req.params.id)
+            res.status(200).send(result)
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    },
+    updateMatter: async (req, res) => {
+        try {
+            const result = await matter_model.findByIdAndUpdate(req.params.id, req.body)
+            res.status(200).send(result)
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    },
+    deleteMatter: async (req, res) => {
+        try {
+            const result = await matter_model.findByIdAndDelete(req.params.id)
+            res.status(200).send(result)
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    }
+}
